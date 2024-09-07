7b427f7fb98e590f79894dc42c65502e1cdedac6
prisis <d.bannert@anolilab.de>
2024-09-07 11:50:40 +0200

    fix: fixed interface change

--- packages/packem/src/rollup/plugins/oxc/isolated-declarations-oxc-transformer.ts
+++ packages/packem/src/rollup/plugins/oxc/isolated-declarations-oxc-transformer.ts
@@ -2,7 +2,14 @@ import { isolatedDeclaration } from "oxc-transform";
 
 import type { IsolatedDeclarationsResult } from "../../../types";
 
-const isolatedDeclarationsOxcTransformer = (id: string, code: string): IsolatedDeclarationsResult => isolatedDeclaration(id, code);
+const isolatedDeclarationsOxcTransformer = (id: string, code: string): IsolatedDeclarationsResult => {
+    const result = isolatedDeclaration(id, code, { sourcemap: false });
+
+    return {
+        errors: result.errors,
+        sourceText: result.code,
+    };
+};
 
 // eslint-disable-next-line import/no-unused-modules
 export default isolatedDeclarationsOxcTransformer;
