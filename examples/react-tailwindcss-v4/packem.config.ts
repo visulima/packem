7fe51d62e32ee82357ee1a9d42fe411d0dfc3d38
prisis <d.bannert@anolilab.de>
2025-01-25 20:06:26 +0100

    chore: added tailwind v4 postcss example

---
+++ examples/react-tailwindcss-v4/packem.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "@visulima/packem/config";
+import transformer from "@visulima/packem/transformer/esbuild";
+import postcssLoader from "@visulima/packem/css/loader/postcss";
+import sourceMapLoader from "@visulima/packem/css/loader/sourcemap";
+import cssnanoMinifier from "@visulima/packem/css/minifier/cssnano";
+
+// eslint-disable-next-line import/no-unused-modules
+export default defineConfig({
+    runtime: "browser",
+    transformer,
+    rollup: {
+        css: {
+            mode: "extract",
+            loaders: [postcssLoader, sourceMapLoader],
+            minifier: cssnanoMinifier
+        },
+    },
+    declaration: "node16"
+});
