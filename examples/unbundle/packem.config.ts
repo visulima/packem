d0e4b699449516c493c6f37eca9cd53295c21533
prisis <d.bannert@anolilab.de>
2025-11-11 19:31:08 +0100

    chore: added example for unbundle

---
+++ examples/unbundle/packem.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "@visulima/packem/config";
+import transformer from "@visulima/packem/transformer/esbuild";
+
+// eslint-disable-next-line import/no-unused-modules
+export default defineConfig({
+    runtime: "node",
+    transformer,
+    unbundle: true,
+});
+
