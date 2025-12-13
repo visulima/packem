25828b2e72c2414c84da3f1497ab47e1719bfb51
prisis <d.bannert@anolilab.de>
2025-12-13 20:45:00 +0100

    fix: improve logic for checking Git status in add command
    - Updated the `checkGitDirty` function to ensure it correctly evaluates the output of the Git command, enhancing reliability.
    - Refactored the return statement for clarity and consistency in handling potential undefined values.

--- packages/packem/src/config/utils/load-preset.ts
+++ packages/packem/src/config/utils/load-preset.ts
@@ -1,10 +1,6 @@
 import type { Jiti } from "jiti";
 
 import type { BuildConfig, BuildPreset } from "../../types";
-import { createReactPreset } from "../preset/react";
-import { createSolidPreset } from "../preset/solid";
-import { createSveltePreset } from "../preset/svelte";
-import { createVuePreset } from "../preset/vue";
 
 const loadPreset = async (preset: BuildPreset | string, jiti: Jiti): Promise<BuildConfig> => {
     switch (preset) {
@@ -12,24 +8,28 @@ const loadPreset = async (preset: BuildPreset | string, jiti: Jiti): Promise<Bui
             return {};
         }
         case "react": {
+            const { createReactPreset } = await import("../preset/react");
             // eslint-disable-next-line no-param-reassign
             preset = createReactPreset();
 
             break;
         }
         case "solid": {
+            const { createSolidPreset } = await import("../preset/solid");
             // eslint-disable-next-line no-param-reassign
             preset = createSolidPreset();
 
             break;
         }
         case "svelte": {
+            const { createSveltePreset } = await import("../preset/svelte");
             // eslint-disable-next-line no-param-reassign
             preset = createSveltePreset();
 
             break;
         }
         case "vue": {
+            const { createVuePreset } = await import("../preset/vue");
             // eslint-disable-next-line no-param-reassign
             preset = createVuePreset();
 
